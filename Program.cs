ab021af31897864c66a6bec520e223afe716b8a7
Роман <ram23@mail.ru>
2022-05-14 14:06:28 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace Module_9
+{
+    class MyException : Exception 
+    {
+        public MyException(string Message) : base(Message) { }
+
+    }
+    class Program
+    {
+        public static void Main()
+        {
+            Exception[] exceptions = new Exception[5]
+            {
+                new DivideByZeroException(),
+                new FormatException(),
+                new FileNotFoundException(),
+                new MyException("Создано свое исключение"),
+                new DriveNotFoundException()
+            };
+                try
+                {
+                    foreach (Exception ex in exceptions)
+                    {
+                        Console.WriteLine("Отработало исключение "+ ex.GetType());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    if (ex is DivideByZeroException) Console.WriteLine(ex.Message);
+                    if (ex is FileNotFoundException) Console.WriteLine(ex.Message);
+                    if (ex is DriveNotFoundException) Console.WriteLine(ex.Message);
+                    if (ex is FormatException) Console.WriteLine(ex.Message);
+                    if (ex is MyException) Console.WriteLine(ex.Message);
+
+                }
+            
+        }
+    }
+}
+
+
+
+
+    
+
