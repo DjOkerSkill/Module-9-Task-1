846453386699ea3313053a8b19b7aa141c03150d
Роман <ram23@mail.ru>
2022-05-15 12:23:19 +0300

    Final Task Module 9

--- Program.cs
+++ Program.cs
@@ -1,42 +1,93 @@
 ﻿using System;
-using System.IO;
+using System.Collections.Generic;
+
 
 namespace Module_9
 {
-    class MyException : Exception 
-    {
-        public MyException(string Message) : base(Message) { }
+    public class SortFamily 
+    { 
+        public delegate void SortDelegate(List<string> vs);
 
+        public event SortDelegate SortEven;
+        
+        public void SortEventHandler(List<string> list) 
+        { 
+             SortEven.Invoke(list);
+        }
     }
+    
     class Program
     {
         public static void Main()
         {
-            Exception[] exceptions = new Exception[5]
+            List<string> list = new List<string>() { "Ivanov", "Petrov", "Zhirov", "Cerman", "Nikolaev" };
+
+            Console.WriteLine("Введите 1 для сортировки спиcка Фамилий от A до Z" + '\n' + "Введите 2 для сортировки списка Фамилий от Z до A");
+
+            int number=0;
+            bool Ischeck = true;
+            
+            while (Ischeck)
             {
-                new DivideByZeroException(),
-                new FormatException(),
-                new FileNotFoundException(),
-                new MyException("Создано свое исключение"),
-                new DriveNotFoundException()
-            };
                 try
                 {
-                    foreach (Exception ex in exceptions)
-                    {
-                        Console.WriteLine("Отработало исключение "+ ex.GetType());
-                    }
+                    number = int.Parse(Console.ReadLine());
+                    Ischeck = false;
                 }
-                catch (Exception ex)
+                catch (FormatException ex)
                 {
-                    if (ex is DivideByZeroException) Console.WriteLine(ex.Message);
-                    if (ex is FileNotFoundException) Console.WriteLine(ex.Message);
-                    if (ex is DriveNotFoundException) Console.WriteLine(ex.Message);
-                    if (ex is FormatException) Console.WriteLine(ex.Message);
-                    if (ex is MyException) Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Введите правильное число 1 или 2");
+                }
+                finally 
+                { 
+                    Console.WriteLine("Значение успешно введено");
+                }
+            }
 
+                    SortFamily sortFamily = new SortFamily();
+
+                switch (number)
+                {
+                    
+                    case 1:
+                        sortFamily.SortEven += SortListAZ;
+                        sortFamily.SortEventHandler(list);
+                        break;
+                    case 2:
+                        sortFamily.SortEven += SortListZA;
+                        sortFamily.SortEventHandler(list);
+                        break;
                 }
             
+        }
+
+        public static void SortListAZ(List<string> list1) 
+        { 
+            list1.Sort();
+
+            foreach (var item in list1)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static void SortListZA(List<string> list2)
+        {
+            list2.Sort();
+
+            List<string> list3 = new List<string>() {"","","","","" };
+
+            for (int i = 0; i < list2.Count; i++)
+            {
+                list3[i] = list2[list2.Count - i - 1];
+            }
+
+            foreach (var item in list3)
+            {
+                Console.WriteLine(item);
+            }
+
         }
     }
 }
